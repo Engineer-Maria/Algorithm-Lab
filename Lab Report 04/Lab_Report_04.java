48bbc397a164e7540433b5d28c0f36662df33af0
Engineer-Maria <mariaptk007@gmail.com>
2025-07-22 01:11:22 +0600

    Add files via upload

---
+++ Lab Report 04/Lab_Report_04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab_report_04;
+
+/**
+ *
+ * @author Dulal
+ */
+public class Lab_Report_04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
